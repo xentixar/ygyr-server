eff4c303d3c715b833f9249f0175fc3e5649a8e6
xentixar <xentixar@gmail.com>
2024-05-10 18:17:15 +0545

    [IMP]:changed application logo and added regiter page link to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,5 +43,12 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+        <div class="mt-5 text-center">
+            <span>Don't have an account? </span>
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('Register') }}
+            </a>
+        </div>
     </form>
 </x-guest-layout>
